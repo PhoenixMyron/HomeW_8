2473a35394f9fe53ced06066a7edfb1f3f14d4e8
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-17 16:10:09 +0600

    task 54 is done

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -8,4 +8,52 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+void InputMatrix(int[, ] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10); // [1, 10]
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+}
+void ChangeRows(int[, ] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 1; j < matrix.GetLength(1); j++)
+{
+    int k = j;
+    while ( k > 0 && matrix[i, k - 1] > matrix[i,k])
+    {
+        int temp = matrix[i, k - 1];
+        matrix[i, k - 1] = matrix[i,k];
+        matrix[i,k] = temp;
+        k -= 1;
+    }
+}
+}
+Console.WriteLine();
+}
 
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+{
+    for (int j = 0; j < matr.GetLength(1); j++) 
+    {
+        Console.Write($"{matr[i, j]} \t");
+    }
+  Console.WriteLine();
+}
+}
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[, ] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+ChangeRows(matrix);
+PrintArray(matrix);
