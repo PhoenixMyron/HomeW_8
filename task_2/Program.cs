7a20c732117a8da5281a679d922a3296aaf0e40b
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-18 01:28:07 +0600

    task 56 is done

---
+++ task_2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и 
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void InputMatrix(int[, ] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10); // [1, 10]
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+{
+    for (int j = 0; j < matr.GetLength(1); j++) 
+    {
+        Console.Write($"{matr[i, j]} \t");
+    }
+  Console.WriteLine();
+}
+}
+
+
+void ChangeRows(int[, ] matrix)
+{
+    int sum = int.MaxValue;
+    int index = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)  
+    {
+        int temp = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            temp += matrix[i, j];
+        }
+        if (temp < sum)
+        {
+            sum = temp;
+            index = i;
+        }
+    }
+Console.WriteLine("Строка: " + ++index );
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[, ] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+ChangeRows(matrix);
+
