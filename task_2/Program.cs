5434aa7efcd10ba7edcd080139bf6e0a2bd44b70
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-18 22:57:47 +0600

    well I've changed task 54 again

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -8,6 +8,65 @@
 // Программа считает сумму элементов в каждой строке и 
 // выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
+// void InputMatrix(int[, ] matrix)
+// {
+//     if (matrix.GetLength(0) == matrix.GetLength(1) )
+//     {
+//         Console.Write("Размеры матрицы не должны быть одинаковыми. \nВведите размеры матрицы: ");
+//         int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+//         matrix = new int[size[0], size[1]];
+//     }
+// for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i, j] = new Random().Next(1, 10); // [1, 10]
+//         Console.Write($"{matrix[i, j]} \t");
+//     }
+//     Console.WriteLine();
+//     }
+// }
+
+// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++) 
+// {
+//     for (int j = 0; j < matr.GetLength(1); j++) 
+//     {
+//         Console.Write($"{matr[i, j]} \t");
+//     }
+//   Console.WriteLine();
+// }
+// }
+
+
+// void ChangeRows(int[, ] matrix)
+// {
+//     int sum = int.MaxValue;
+//     int index = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)  
+//     {
+//         int temp = 0;
+//         for (int j = 0; j < matrix.GetLength(1); j++) 
+//         {
+//             temp += matrix[i, j];
+//         }
+//         if (temp < sum)
+//         {
+//             sum = temp;
+//             index = i;
+//         }
+//     }
+// Console.WriteLine("Строка: " + ++index );
+// }
+
+// Console.Clear();
+// Console.Write("Введите размеры матрицы: ");
+// int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// int[, ] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+// ChangeRows(matrix);
+
 void InputMatrix(int[, ] matrix)
 {
     if (matrix.GetLength(0) == matrix.GetLength(1) )
@@ -57,7 +116,7 @@ void ChangeRows(int[, ] matrix)
             index = i;
         }
     }
-Console.WriteLine("Строка: " + ++index );
+Console.WriteLine("Строка с наименьшой суммой элементов: " + ++index );
 }
 
 Console.Clear();
@@ -66,4 +125,3 @@ int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[, ] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
 ChangeRows(matrix);
-
