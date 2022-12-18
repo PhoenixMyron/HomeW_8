19537a93792ea9736e2b2652586e1947d9ecbc7a
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-18 17:13:13 +0600

    task 58 is in proccess

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// Задача 58: Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+void InputMatrix(int[, ] matrix)
+{
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[, ] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
