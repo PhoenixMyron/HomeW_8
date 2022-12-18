d45e1d4466be12c4009e511dac441d40516e7176
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-18 22:56:08 +0600

    task 58 is done

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -6,22 +6,85 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
-void InputMatrix(int[, ] matrix)
+void RandomMatrix(int[, ] matrix)
 {
-
 for (int i = 0; i < matrix.GetLength(0); i++)
-    {
+{
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        Console.Write($"{matrix[i, j]} \t");
-    }
-    Console.WriteLine();
+        matrix[i, j] = new Random().Next(1, 10); // [1, 10];
     }
 }
+}
+void RandomSecondMatrix(int[, ] SecondMatrix)
+{
+for (int i = 0; i < SecondMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < SecondMatrix.GetLength(1); j++)
+    {
+        SecondMatrix[i, j] = new Random().Next(1, 10); // [1, 10];
+    }
+}
+}
 
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+{
+    for (int j = 0; j < matr.GetLength(1); j++) 
+    {
+        Console.Write($"{matr[i, j]} \t");
+    }
+  Console.WriteLine();
+}
+}
+void PrintSecondMatrix(int[,] SecondMatrix)
+{
+    for (int i = 0; i < SecondMatrix.GetLength(0); i++) 
+{
+    for (int j = 0; j < SecondMatrix.GetLength(1); j++) 
+    {
+        Console.Write($"{SecondMatrix[i, j]} \t");
+    }
+  Console.WriteLine();
+}
+}
+void PrintResultMatrix(int[,] ResultMatrix)
+{
+    for (int i = 0; i < ResultMatrix.GetLength(0); i++) 
+{
+    for (int j = 0; j < ResultMatrix.GetLength(1); j++) 
+    {
+        Console.Write($"{ResultMatrix[i, j]} \t");
+    }
+  Console.WriteLine();
+}
+}
+void SummOfMatrix(int[, ] matrix,int[, ] SecondMatrix,int[, ] ResultMatrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        int temp = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            temp = matrix[i,j] * SecondMatrix[i,j];
+            ResultMatrix[i,j] = temp;
+        }
+    }
 
+}
 Console.Clear();
 Console.Write("Введите размеры матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[, ] matrix = new int[size[0], size[1]];
-InputMatrix(matrix);
+int[, ] SecondMatrix = new int[size[0], size[1]];
+int[, ] ResultMatrix = new int[size[0], size[1]];
+RandomMatrix(matrix);
+RandomSecondMatrix(SecondMatrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+PrintSecondMatrix(SecondMatrix);
+Console.WriteLine();
+SummOfMatrix(matrix,SecondMatrix,ResultMatrix);
+PrintResultMatrix(ResultMatrix);
+
