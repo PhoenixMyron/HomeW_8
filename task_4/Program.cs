8e3543b86951ea4b5cef3f1e3ab3ea93d59fe18e
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-19 12:47:39 +0600

    task 60 is done

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -6,3 +6,37 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+void RandomMatrix(int[, , ] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(2); k++)
+        {
+            matrix[i, j, k] = new Random().Next(10, 100); 
+        }
+    }
+}
+}
+void PrintMatrix(int[, , ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+{
+    for (int j = 0; j < matrix.GetLength(1); j++) 
+    {
+       for (int k = 0; k < matrix.GetLength(2); k++)
+       {
+        Console.Write($"{matrix[i, j, k]}  ({i},{j},{k}) \t");
+
+       }
+       Console.WriteLine();
+    }
+}
+}
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[, , ] matrix = new int[size[0], size[1], size[2]];
+RandomMatrix(matrix);
+PrintMatrix(matrix);
